6d10bf340f447d26efb6f751e6b18205a95cd1a1
ahsan <ahsan_zami@hotmail.com>
2018-12-31 11:34:02 +0400

    *Query string version fix for production build

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -258,6 +258,23 @@ module.exports = {
         new webpack.DefinePlugin(env.stringified),
         // Minify the code.
         // Note: this won't work without ExtractTextPlugin.extract(..) in `loaders`.
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false,
+                // Disabled because of an issue with Uglify breaking seemingly valid code:
+                // https://github.com/facebookincubator/create-react-app/issues/2376
+                // Pending further investigation:
+                // https://github.com/mishoo/UglifyJS2/issues/2011
+                comparisons: false,
+            },
+            output: {
+                comments: false,
+                // Turned on because emoji and regex is not minified properly using default
+                // https://github.com/facebookincubator/create-react-app/issues/2488
+                ascii_only: true,
+            },
+            sourceMap: shouldUseSourceMap,
+        }),
         new ExtractTextPlugin({
             filename: cssFilename,
         }),
